d5f4e337c91afb01865416b6abc40dacdf0ed0c0
Alexander Antipov <antipovaleksandr95@gmail.com>
2018-08-28 22:00:16 +0300

    added displaying map

--- app/src/main/java/com/antipov/buildaroute/ui/base/BaseFragment.java
+++ app/src/main/java/com/antipov/buildaroute/ui/base/BaseFragment.java
@@ -2,6 +2,7 @@ package com.antipov.buildaroute.ui.base;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
@@ -83,6 +84,14 @@ public abstract class BaseFragment extends Fragment implements IBaseView {
     @Nullable
     public BaseActivity getBaseActivity() {
         return mActivity;
+    }
+
+
+    /**
+     * @return inflated view
+     */
+    public View getInflatedView() {
+        return mView;
     }
 
     /**
--- app/src/main/java/com/antipov/buildaroute/ui/fragment/map/MapFragment.java
+++ app/src/main/java/com/antipov/buildaroute/ui/fragment/map/MapFragment.java
@@ -1,9 +1,60 @@
 package com.antipov.buildaroute.ui.fragment.map;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
 import com.antipov.buildaroute.R;
 import com.antipov.buildaroute.ui.base.BaseFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends BaseFragment{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MapFragment extends BaseFragment implements OnMapReadyCallback {
+
+    @BindView(R.id.map) MapView map;
+    private GoogleMap googleMap;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        map.onStart();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        map.onCreate(savedInstanceState);
+        map.getMapAsync(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        map.onStop();
+    }
+
     @Override
     public int getLayoutId() {
         return R.layout.fragment_map;
@@ -16,7 +67,7 @@ public class MapFragment extends BaseFragment{
 
     @Override
     public void initViews() {
-
+        ButterKnife.bind(this, getInflatedView());
     }
 
     @Override
@@ -32,5 +83,18 @@ public class MapFragment extends BaseFragment{
     @Override
     public void hideLoadingFullscreen() {
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        // For dropping a marker at a point on the Map
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+        // For zooming automatically to the location of the marker
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
